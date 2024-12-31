bd75e90ab6fa6ab22e97c5a654d0788fbe8395c3
Aleksander Da Silva Toth <aleksander_toth@outlook.com>
2024-12-30 22:21:55 -0300

    Finalizada primeira parte, com variáveis de ambiente

--- Introducao/TODO.md
+++ Introducao/TODO.md
@@ -1,10 +1,10 @@
 # Aula introdutória
 
-- [ ] Módulo I/O
+- [x] Módulo I/O
     - [x] Prompt
     - [x] Arquivos
-- [ ] Módulo Console
-- [ ] Env
+- [x] Módulo Console (console.log, console.table, console.dir, console.error, console.time. console.timeEnd)
+- [x] Env (variáveis de ambiente. Usar: npm init -y, npm i dotenv)
 - [ ] Módulos de Rede
     - [ ] DNS
     - [ ] http
--- Introducao/arquivos.js
+++ Introducao/arquivos.js
@@ -11,13 +11,30 @@ const fs = require('node:fs')
 const filePath = path.join(process.cwd(),'Introducao', 'texto.txt')
 const fileOutPath = path.join(process.cwd(),'Introducao', 'texto-com-linhas.txt')
 
+const dotenv = require('dotenv')
+dotenv.config()
+console.log(process.env.NODE_ENV)
+
+// Exibindo o caminho do arquivo com o console.log
+// o método log exibe uma mensagem no console
+console.log(filePath)
+
+// Iniciando um contador de tempo, utilizando o console.time
+console.time('Manipular Arquivos')
+console.time('Leitura do Arquivo')
+
 // Lendo o arquivo texto.txt
 fs.readFile(filePath, {}, (erro, dados) => {
     // Se houver erro, exibir mensagem de erro
     if (erro) {
-        console.log(`Erro na leitura do arquivo no caminho: ${filePath}`)
+        //console.log(`Erro na leitura do arquivo no caminho: ${filePath}`)
+        // Com o console.error, a mensagem de erro é exibida em vermelho
+        console.error(`Erro na leitura do arquivo no caminho: ${filePath}`)
         return
     }
+
+    // Exibindo o tempo de leitura do arquivo
+    console.timeEnd('Leitura do Arquivo')
     // Separando as linhas do arquivo
     // O método split divide uma string em um array de substrings
     const texto = dados.toString()
@@ -33,14 +50,23 @@ fs.readFile(filePath, {}, (erro, dados) => {
     // Split: divide uma string em um array de substrings
     fs.writeFile(fileOutPath, linhasAjustadas.join('\n'), {}, (erro) => {
         if (erro) {
-            console.log(`Erro na escrita do arquivo no caminho: ${fileOutPath}`)
+            console.error(`Erro na escrita do arquivo no caminho: ${fileOutPath}`)
             return
         }
+        // Quando utilizamos objeto, se usarmos o console.dir, ele exibe o objeto de forma, mais autoexplicativa
+        // Ou também o console.table, que exibe o objeto em forma de tabela
         console.log(`Arquivo escrito com sucesso no caminho: ${fileOutPath}`)
+
+        console.log(`Arquivo salvo no bucket ${process.env.S3_BUCKET}`)
+
+        // As variáveis de ambiente são muito importantes, pois com elas podemos configurar o ambiente de execução
+        // ou seja, não precisamos alterar o código, apenas as variáveis de ambiente, que podem ser
+        // em um sistema operacional, um bucket diferente, na nuvem, ambiente de desenvolvimento,
+        // ambiente de produção, ambiente de teste
+
+        console.timeEnd('Manipular Arquivos')
     })
     // Exibindo o conteúdo do arquivo
     //console.log(dados.toString())  
 })
 
-// Exibindo o caminho do arquivo
-console.log(filePath)
